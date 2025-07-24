1e3c71c66b2f1808d27b47636adf82e74faa6bc0
rahuwul <rahuwul@gmail.com>
2025-07-24 23:50:56 +0530

    implement clerk pricing table

---
+++ app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import { PricingTable } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (<main className='flex items-center justify-center '> 
+    <PricingTable />
+    </main>
+  )
+}
+
+export default page
