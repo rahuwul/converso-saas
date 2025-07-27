2f63855be41ea030a046c1c6aefa891bb97e2e18
rahuwul <rahuwul@gmail.com>
2025-07-28 05:17:01 +0530

    implement search functionality

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,37 @@
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import SearchFilter from "@/components/SearchFilter";
+import SearchInput from "@/components/SearchInput";
 
-const CompanionsLibrary = () => {
+async function CompanionsLibrary({ searchParams }: SearchParams) {
+  const filters = await searchParams;
+
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+
+  console.log('Companions:', companions);
+
   return (
-    <div>CompanionsLibrary</div>
-  )
+    <main>
+      <section>
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SearchFilter />
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id} {...companion}
+            color={getSubjectColor(companion.subject)} />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default CompanionsLibrary;
