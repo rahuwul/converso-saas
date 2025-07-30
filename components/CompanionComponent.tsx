d0f5f15f3f1cc87dfb53603bcd2e536f6879458d
rahuwul <rahuwul@gmail.com>
2025-07-31 04:39:00 +0530

    implement vapi.ai

---
+++ components/CompanionComponent.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { cn, getSubjectColor } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import { error } from "console";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  ACTIVE = "ACTIVE",
+  CONNECTING = "CONNECTING",
+  FINISHED = "FINISHED",
+}
+
+const CompanionComponent = ({
+  companionId,
+  subject,
+  topic,
+  name,
+  userName,
+  userImage,
+  style,
+  voice,
+}: CompanionComponentProps) => {
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
+  useEffect(() => {
+    const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+    const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+
+    const onMessage = () => {};
+    const onError = (error: Error) => console.log("Error", Error);
+
+    const onSpeechStart = () => setIsSpeaking(true);
+    const onSpeechEnd = () => setIsSpeaking(false);
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("error", onError);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("error", onError);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+    };
+  }, []);
+
+  return (
+    <section className="flex flex-col has-[70vh]:">
+      <section className="flex gap-8 max-sm:flex-col ">
+        <div className="companion-section">
+          <div
+            className="companion-avatar"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <div
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                callStatus === CallStatus.INACTIVE ||
+                  callStatus === CallStatus.ACTIVE
+                  ? "opacity-1001"
+                  : "opacity-0",
+                callStatus === CallStatus.INACTIVE &&
+                  "opacity-100 animate-pulse"
+              )}
+            >
+              <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className="max-sm:w-fit"/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CompanionComponent;
